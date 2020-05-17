0bb71cc53e0f72a0726f41f79b82ce78f7f60495
hxy112292 <983771687@qq.com>
2020-05-17 04:14:12 -0500

    修改显示编码

--- src/app/entity/coin-search-result.ts
+++ src/app/entity/coin-search-result.ts
@@ -3,6 +3,7 @@ export class CoinSearchResult {
     coincode: string;
     coinname: string;
     coinlogo: string;
+    symbol: string;
     // tslint:disable-next-line:variable-name
     market_value: string;
     // tslint:disable-next-line:variable-name
