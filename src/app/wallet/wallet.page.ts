b759ef5cfcf1413e2d9879680d0f92460bb15f5c
hxy112292 <983771687@qq.com>
2020-05-27 01:08:21 -0500

    解决钱包无法增加的bug

--- src/app/wallet/wallet.page.ts
+++ src/app/wallet/wallet.page.ts
@@ -29,7 +29,7 @@ export class WalletPage implements OnInit {
   }
 
   toAddWallet() {
-    this.router.navigate(['tabs/wallet/wallet-create-choose']);
+    this.router.navigate(['tabs/wallet/wallet-add-choose']);
   }
 
   toWalletManagement(privateKey: PrivateKey, i: number) {
