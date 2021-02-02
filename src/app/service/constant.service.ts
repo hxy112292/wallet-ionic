e42cb1518f3d25997138885511093d15fcc15b79
hxy112292 <983771687@qq.com>
2021-02-03 02:10:21 +0800

    添加灰度基金数据

--- src/app/service/constant.service.ts
+++ src/app/service/constant.service.ts
@@ -6,8 +6,8 @@ import { Injectable } from '@angular/core';
 export class ConstantService {
 
   readonly walletBackendUrl: string = 'https://www.hd-wallet.com:7070';
-  // readonly walletToolBackendUrl: string = 'https://www.hd-wallet.com:7071';
-  readonly walletToolBackendUrl: string = 'http://localhost:7071';
+  readonly walletToolBackendUrl: string = 'https://www.hd-wallet.com:7071';
+  // readonly walletToolBackendUrl: string = 'http://localhost:7071';
   readonly webUrl: string = 'https://www.hd-wallet.com';
   readonly githubUrl: string = 'https://github.com/hxy112292/wallet-ionic';
   readonly litecoreTestnetUrl: string = 'https://testnet.litecore.io';
